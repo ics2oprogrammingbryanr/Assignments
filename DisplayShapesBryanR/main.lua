37a99607a1834fbd130309d788849b47a6081209
Bryan Rodriguez <47784159+s267522@users.noreply.github.com>
2019-02-26 09:47:16 -0500

    Initial commit

---
+++ DisplayShapesBryanR/main.lua
@@ -0,0 +1,14 @@
+-- Title: DisplayingShapesInLua 
+
+local areaText
+local textSize
+local polygonVertices = 
+
+-- set couler of background
+display.setDefault("background", 17/255, 86/255, 10/255)
+
+-- remove status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--create polygon
+display.newPolygon()
