add2cef15e483b2bb9a90c8acae83a57df62d088
Bryan Rodriguez <47784159+s267522@users.noreply.github.com>
2019-02-28 09:14:22 -0500

    Added Animating Images

--- DisplayShapesBryanR/main.lua
+++ DisplayShapesBryanR/main.lua
@@ -2,13 +2,38 @@
 
 local areaText
 local textSize
-local polygonVertices = 
+local TriangleVertices = { 0,-150, 150,150, 150,-150 }
+local myTriangle
+local starVertices = { -150,-150, 0,-50, 150,-150, 50,0, 150,150,
+ 0,50, -150,150, -50,0 }
+ local myStar
 
 -- set couler of background
-display.setDefault("background", 17/255, 86/255, 10/255)
+display.setDefault("background", 56/255, 200/255, 10/255)
 
 -- remove status bar
 display.setStatusBar(display.HiddenStatusBar)
 
---create polygon
-display.newPolygon()
+--create triangle
+myTriangle = display.newPolygon(850, 400 ,TriangleVertices)
+
+--create border
+myTriangle.strokeWidth = 15
+
+--set color of border
+myTriangle:setStrokeColor(0, 0, 0)
+
+--set color of triangle
+myTriangle:setFillColor(108/255, 32/255, 159/255)
+
+--create poltgon
+myStar = display.newPolygon(550, 400 ,starVertices)
+
+--set border for star
+myStar.strokeWidth = 15
+
+--set color for star
+myStar:setFillColor(138/255, 138/255, 138/255)
+
+--set color of border
+myStar:setStrokeColor(0, 0, 0)
