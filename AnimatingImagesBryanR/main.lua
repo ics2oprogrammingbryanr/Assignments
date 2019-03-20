e148d7c6d1f1d62b29375ea1b0914221c9d5c3f0
Bryan Rodriguez <47784159+s267522@users.noreply.github.com>
2019-03-20 09:10:47 -0400

    updated animating images

--- AnimatingImagesBryanR/main.lua
+++ AnimatingImagesBryanR/main.lua
@@ -1,7 +1,65 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: AnimatingImages
+-- Name: Bryan Rodriguez
 
--- Your code here
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- global variables
+scrollspeed = 3
+scrollspeed2 = 6
+-- background image with width and height
+local backgrondgroundImage = display.newImageRect("Images/Grass.jfif", 2048, 1536)
+
+-- create character with width and height
+local hummingbird = display.newImageRect("Images/hummingbird.png", 200, 200)
+
+--set initial x and y position of hummingbird
+hummingbird.x = 0
+hummingbird.y = display.contentHeight/2
+
+-- create bird with width and height
+local bird = display.newImageRect("Images/Bird.png", 200, 200)
+
+-- set initial x and y position of bird
+bird.x = 0
+bird.y = display.contentHeight/4
+
+-- set images to be transparent
+hummingbird.alpha = 0
+bird.alpha = 0
+
+-- Function: MoveHummingbird
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to the x-value of the hummingbird
+local function MoveHummingbird(event)
+	-- add scroll speed to the x_value of the hummingbird
+	hummingbird.x = hummingbird.x + scrollspeed2
+	-- change the transparency of the hummingbird every time it moves so that it fades out
+	hummingbird.alpha = hummingbird.alpha + 0.01
+end
+
+-- MoveHummingbird will be called over and over again
+Runtime:addEventListener("enterFrame", MoveHummingbird)
+
+-- Function: MoveBird
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: this function adds the scroll speed to the x-value of the bird
+local function MoveBird(event)
+	-- add scroll speed to the x-value of the bird
+	bird.x = bird.x + scrollspeed
+	-- change the transparency of bird every time it move so that it fades out
+	bird.alpha = bird.alpha + 0.01
+end
+
+-- MoveBird will be called over and over again
+Runtime:addEventListener("enterFrame", MoveBird)
+
+-- Make it so the birds stop in the middle
+if (bird.x >= 1024) then
+	scrollspeed = -3
+end
+
+bird.x = bird.x + scrollspeed
+
--- DisplayShapesBryanR/main.lua
+++ DisplayShapesBryanR/main.lua
@@ -1,12 +1,16 @@
 -- Title: DisplayingShapesInLua 
 
 local areaText
-local textSize
+local textSize = 50
 local TriangleVertices = { 0,-150, 150,150, 150,-150 }
 local myTriangle
 local starVertices = { -150,-150, 0,-50, 150,-150, 50,0, 150,150,
  0,50, -150,150, -50,0 }
  local myStar
+ local vertices = { 0,-150, 150,150, 100,200, 50,250, -50,250, -100,200, -150,150 }
+ local myCone
+ local newAreaText
+ local newAreaText2
 
 -- set couler of background
 display.setDefault("background", 56/255, 200/255, 10/255)
@@ -37,3 +41,36 @@ myStar:setFillColor(138/255, 138/255, 138/255)
 
 --set color of border
 myStar:setStrokeColor(0, 0, 0)
+
+--create cone 
+myCone = display.newPolygon(200, 350, vertices)
+
+--set cone color
+myCone:setFillColor(20/255, 238/255, 26)
+
+--set cone border
+myCone.strokeWidth = 15
+
+--set border color
+myCone:setStrokeColor(0, 0, 0)
+
+--create new text
+areaText = display.newText("triangle", 0, 0, Arial, textSize)
+
+--set x and y position
+areaText.x = 900
+areaText.y = 600
+
+--create new text 
+newAreaText = display.newText("star", 0, 0, Arial, textSize)
+
+--set x and y position
+newAreaText.x = 550
+newAreaText.y = 600
+
+--create pentagon
+newAreaText2 = display.newText("pentagon", 0, 0, Arial, textSize)
+
+--set x and y position
+newAreaText2.x = 200
+newAreaText2.y = 600
