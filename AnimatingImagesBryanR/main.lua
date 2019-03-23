ac339f09a7e956507e83c2b9d2a38f972976faf6
Bryan Rodriguez <47784159+s267522@users.noreply.github.com>
2019-03-22 20:49:27 -0400

    animating images updated

--- AnimatingImagesBryanR/main.lua
+++ AnimatingImagesBryanR/main.lua
@@ -15,18 +15,26 @@ local hummingbird = display.newImageRect("Images/hummingbird.png", 200, 200)
 
 --set initial x and y position of hummingbird
 hummingbird.x = 0
-hummingbird.y = display.contentHeight/2
+hummingbird.y = 650
 
 -- create bird with width and height
 local bird = display.newImageRect("Images/Bird.png", 200, 200)
 
 -- set initial x and y position of bird
 bird.x = 0
-bird.y = display.contentHeight/4
+bird.y = 768
+
+-- create cycle with width and height
+local cycle = display.newImageRect("Images/cycle.png", 200, 200)
+
+-- set initial x and y position of cycle
+cycle.x = 0
+cycle.y = 768
 
 -- set images to be transparent
 hummingbird.alpha = 0
 bird.alpha = 0
+cycle.alpha = 0
 
 -- Function: MoveHummingbird
 -- Input: this function accepts an event listener
@@ -35,8 +43,10 @@ bird.alpha = 0
 local function MoveHummingbird(event)
 	-- add scroll speed to the x_value of the hummingbird
 	hummingbird.x = hummingbird.x + scrollspeed2
+	-- add scrollspeed to the y-value of hummingbird to make it go diagonall
+	hummingbird.y = hummingbird.y - scrollspeed2
 	-- change the transparency of the hummingbird every time it moves so that it fades out
-	hummingbird.alpha = hummingbird.alpha + 0.01
+	hummingbird.alpha = hummingbird.alpha + 0.02
 end
 
 -- MoveHummingbird will be called over and over again
@@ -49,6 +59,8 @@ Runtime:addEventListener("enterFrame", MoveHummingbird)
 local function MoveBird(event)
 	-- add scroll speed to the x-value of the bird
 	bird.x = bird.x + scrollspeed
+	-- add scrollspeed to the y-value of bird to make it go diagonally
+	bird.y = bird.y - scrollspeed
 	-- change the transparency of bird every time it move so that it fades out
 	bird.alpha = bird.alpha + 0.01
 end
@@ -56,10 +68,23 @@ end
 -- MoveBird will be called over and over again
 Runtime:addEventListener("enterFrame", MoveBird)
 
--- Make it so the birds stop in the middle
-if (bird.x >= 1024) then
-	scrollspeed = -3
+local function MoveCycle(event)
+	-- add scrollspeed to the value of cycle
+    cycle.x = cycle.x + scrollspeed2
+	-- add scrollspeed to the y-value of cycle to make it go diagonal
+	cycle.y = cycle.y - scrollspeed2
+    -- change the transparency of the cycle every time it moves so that it fades out
+    cycle.alpha = cycle.alpha + 0.02
 end
 
-bird.x = bird.x + scrollspeed
+-- MoveCycle will be called over and over again
+Runtime:addEventListener("enterFrame", MoveCycle)
 
+local textObject = display.newText("VROOM VROOM!", 0, 0, nil, 50)
+textObject.x = display.contentWidth/2
+textObject.y = 50
+textObject:setTextColor (3, 0, 0)
+
+local textObject = display.newText("HEY,WAIT UP!!!", 0, 0, nil, 50)
+textObject.x = 250
+textObject.y = 700
